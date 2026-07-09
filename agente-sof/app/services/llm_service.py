4c9c229081aa9a69f595a8d5b32a2b60f0750705
yurilima-sof <yuri.lima@sof.eco.br>
2026-07-09 16:31:17 -0300

    Mundaça da versão de llm utilizada

--- agente-sof/app/services/llm_service.py
+++ agente-sof/app/services/llm_service.py
@@ -109,11 +109,11 @@ class LLMService:
                 f"Mensagem atual do Usuário: '{mensagem}'"
             )
 
-        logger.info(f"   Enviando requisição ao gemini-2.0-flash...")
+        logger.info(f"   Enviando requisição ao gemini-flash-latest...")
 
         # 4. Envia para a API do Gemini de forma assíncrona usando GenerativeModel
         model = genai.GenerativeModel(
-            model_name="gemini-2.0-flash",
+            model_name="gemini-flash-latest",
             system_instruction=system_prompt
         )
         response = await model.generate_content_async(
